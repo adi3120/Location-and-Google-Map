ad6c822ad0fc71fbcc49e783adf522df463135ba
adi3120 <aditya.yadav.cs.20@ggits.net>
2022-09-04 22:13:06 +0530

    Checked if Google Play Services are available or not

--- app/src/main/java/com/example/markers/MainActivity.java
+++ app/src/main/java/com/example/markers/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.markers;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -26,7 +30,33 @@ boolean isPermissionGranted;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         checkPermission();
+        if(isPermissionGranted){
+            if(checkGooglePlayServices()){
+                Toast.makeText(this, "Google Play Services available", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(this, "Google Play Services NOT available", Toast.LENGTH_SHORT).show();
+            }
+        }
 
+    }
+
+    private boolean checkGooglePlayServices() {
+        GoogleApiAvailability googleApiAvailability=GoogleApiAvailability.getInstance();
+        int result=googleApiAvailability.isGooglePlayServicesAvailable(this);
+        if(result== ConnectionResult.SUCCESS){
+            return true;
+        }
+        else if (googleApiAvailability.isUserResolvableError(result)){
+            Dialog dialog=googleApiAvailability.getErrorDialog(this, result, 201, new DialogInterface.OnCancelListener() {
+                @Override
+                public void onCancel(DialogInterface dialogInterface) {
+                    Toast.makeText(MainActivity.this, "User Cancelled Dialog", Toast.LENGTH_SHORT).show();
+                }
+            });
+            dialog.show();
+        }
+        return false;
     }
 
     private void checkPermission() {
