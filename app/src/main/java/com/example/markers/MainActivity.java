c74232b62f56eb2625381822e12b849e278107b1
adi3120 <aditya.yadav.cs.20@ggits.net>
2022-09-04 22:48:16 +0530

    Showed Google Map using MapView and added api Key to project

--- app/src/main/java/com/example/markers/MainActivity.java
+++ app/src/main/java/com/example/markers/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.markers;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
@@ -8,12 +9,15 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.provider.Settings;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.karumi.dexter.Dexter;
@@ -23,16 +27,22 @@ import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 boolean isPermissionGranted;
+MapView mapView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mapView=findViewById(R.id.mapView);
+
         checkPermission();
         if(isPermissionGranted){
             if(checkGooglePlayServices()){
                 Toast.makeText(this, "Google Play Services available", Toast.LENGTH_SHORT).show();
+                mapView.getMapAsync(this);
+                mapView.onCreate(savedInstanceState);
+
             }
             else{
                 Toast.makeText(this, "Google Play Services NOT available", Toast.LENGTH_SHORT).show();
@@ -81,5 +91,60 @@ boolean isPermissionGranted;
                 permissionToken.continuePermissionRequest();
             }
         }).check();
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    //    @Override
+//    protected void onSaveInstanceState(@NonNull Bundle outState,) {
+//        super.onSaveInstanceState(outState);
+//        mapView.onSaveInstanceState(outState);
+//    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
